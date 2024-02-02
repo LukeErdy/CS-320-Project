8c8f54f1d5425ec0106778cdbf639ae82f476b91
Gabrielle-Barling <156250200+Gabrielle-Barling@users.noreply.github.com>
2024-02-02 15:27:27 -0800

    Added the basics of class for dataConsent
    I added the basics of the class. However, I still need to implement AskForConsent.

---
+++ Demo/Assets/DataConsent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Unity.Services.Core;
+using Unity.Services.Analytics;
+
+public class DataConsent : MonoBehaviour
+{
+    async void Start()
+    {
+		await UnityServices.InitializeAsync();
+
+		AskForConsent();
+    }
+
+	void AskForConsent()
+	{
+		// show the player a UI element that asks for consent.
+
+		// if consent given take down UI element and call on ConsentGiven
+
+		// else take down UI element and do not collect data 
+	}
+
+	void ConsentGiven()
+	{
+		AnalyticsService.Instance.StartDataCollection();
+	}
+}
