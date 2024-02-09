4234619a616d79879f011aef9ad60b3e643cec30
Caleb <calebjbiggs@gmail.com>
2024-02-09 15:18:32 -0800

    Created a script that generates levels with grassy hills

--- Demo/Assets/Generate.cs
+++ Demo/Assets/Generate.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -11,64 +12,87 @@ public class Generate : MonoBehaviour
     const int MIN_Y = -22;
     const int MAX_Y = 19;
 
+    const int MAX_AMP = 10;
+    const int MIN_AMP = 3;
+    const int MAX_PER = 10;
+    const int MIN_PER = 5;
+
     //TODO replace this method 2 from https://stackoverflow.com/a/56604959
     public Tilemap tilemap;
     public Tile grass_1;
+    public Tile dirt_1;
     public Tile sky_1;
     public Tile testtile_1;
 
     // Start is called before the first frame update
     void Start()
     {
+        //TODO: Set player position above ground level
+
         //Create 2D array of bytes that will be converted into the tilemap
+        //Position (0,0) corresponds to the bottom left corner in-game
         const int xDim = MAX_X-MIN_X;
         const int yDim = MAX_Y-MIN_Y;
         byte[,] map = new byte[xDim, yDim];
 
-        //Draw a box around the map
-        for(int i = 0; i < xDim; i++){
-            map[i,0] = 1;
-            map[i,yDim-1] = 1;
-        }
-        for(int j = 0; j < yDim; j++){
-            map[0,j] = 1;
-            map[xDim-1,j] = 1;
+        
+        //Generates a grassy, hilly terrain with dirt beneath it
+        //TODO: Make sure hills don't go out the top or bottom of map
+        int startY = 20, width, amplitude;
+        bool startBool = false;
+        var rand = new System.Random();
+
+        for(int i = 0; i < xDim-1; i += width+1){
+            width = rand.Next(MIN_PER, MAX_PER);
+            amplitude = rand.Next(MIN_AMP, MAX_AMP);
+
+            var nextHill = new Hill(width, amplitude, startBool);
+            for(int j = 0; j <= width; j++){
+                if(i+j >= xDim-1) break;
+                int colH = startY+nextHill.getY(j);
+                map[i+j, colH] = 1;
+                for(int k = colH-1; k >= 0; k--) map[i+j,k] = 2;
+            }
+
+            startY += nextHill.getY(width);
+            startBool = !startBool;
         }
 
         //Convert 2D array into tilemap
+        //TODO: Convert values to enum
         for(int i = 0; i < xDim; i++){
             for(int j = 0; j < yDim; j++){
-                if(map[i,j] == 1) tilemap.SetTile(new Vector3Int(i+MIN_X, j+MIN_Y, 0), testtile_1);
+                if(map[i,j] == 1) tilemap.SetTile(new Vector3Int(i+MIN_X, j+MIN_Y, 0), sky_1);
+                switch(map[i,j]){
+                    case 0: tilemap.SetTile(new Vector3Int(i+MIN_X, j+MIN_Y, 0), null); break;
+                    case 1: tilemap.SetTile(new Vector3Int(i+MIN_X, j+MIN_Y, 0), grass_1); break;
+                    case 2: tilemap.SetTile(new Vector3Int(i+MIN_X, j+MIN_Y, 0), dirt_1); break;
+                    case 3: tilemap.SetTile(new Vector3Int(i+MIN_X, j+MIN_Y, 0), sky_1); break;
+                    case 4: tilemap.SetTile(new Vector3Int(i+MIN_X, j+MIN_Y, 0), testtile_1); break;
+                }
             }
         }
-
-
-
-
-        // Debug.Log("hello world");
-        
-        // Vector3Int loc = new Vector3Int(0,0,0);
-        // tilemap.SetTile(loc, grass_1);
-
-        //Test code taken from https://stackoverflow.com/a/56604959
-        // for(int i = 0; i < 20; i++){
-        //     for(int j = 0; j < 20; j++){
-        //         bool odd = ((i+j)%2 == 1);
-        //         tilemap.SetTile(new Vector3Int(i,j,0), odd ? grass_1 : sky_1);
-        //     }
-        // }
-
-        // for(int i = MIN_X; i < MAX_X; i++){
-            // for(int j = -22; j < 19; j++){
-                // tilemap.SetTile(new Vector3Int(i,-2,0), testtile_1);
-                // tilemap.SetTile(new Vector3Int(0,j,0), testtile_1);
-            // }
-        // } 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){}
+}
+
+
+class Hill{
+    float w;
+    float a;
+    bool inv;
+
+    public Hill(int width, int amplitude, bool invert){
+        w = width;
+        a = amplitude;
+        inv = invert;
+    }
+
+    public int getY(int x){
+        int output = (int)((a/2) * (Math.Sin((Math.PI/w)*(x - (w/2))) + 1));
+        if(inv) return (int)(-output);
+        else return output;
     }
 }
