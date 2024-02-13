f5827b23e8e65fcc0f1da3ccd7bbe30e35eaced4
Caleb <calebjbiggs@gmail.com>
2024-02-13 10:50:55 -0800

    Started implementing wave function collapse
    Created a WFCRules class that can specify what tiles can be placed next to each other and created a priority queue for the collapse algorithm to use when generating the tilemap, still needs to be tested.

--- Demo/Assets/Generate.cs
+++ Demo/Assets/Generate.cs
@@ -8,7 +8,8 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 
-enum Tl{ empty, grass_0, dirt_0, stone_0, gravel_0 }
+
+
 
 
 public class Generate : MonoBehaviour
@@ -100,13 +101,130 @@ class Hill{
 }
 
 
+enum Tl{ empty, grass_0, dirt_0, stone_0, gravel_0 }
+
+
+//A WFCRule indicates all the possible tiles that can be placed in
+//the x,y position relative to the current tile
+//TODO: implement weights for tiles
 class WFCRule{
-    (int,int)[] required;
-    (int,int)[] forbidden;
+    int x, y;
+    HashSet<Tl> tiles;
+    public WFCRule(int xIn, int yIn, HashSet<Tl> tilesIn){ x=xIn; y=yIn; tiles=tilesIn; }
+}
+
+
+class WFCQueue{
+    int[,] indices;
+    int size;
+    (int x, int y, HashSet<Tl> tiles)[] queue;
+
+    public WFCQueue(int x, int y){
+        indices = new int[x,y];
+        queue = new (int x, int y, HashSet<Tl> tiles)[x*y];
+        size = queue.Length;
+        //Initialize the queue with each tile being able to be any of
+        //the possible types and the indices pointing to the correct
+        //location in the queue
+        for(int i = 0; i < x; i++){
+            for(int j = 0; j < y; j++){
+                indices[i,j] = i*y + j;
+                queue[indices[i,j]] = (i, j, new HashSet<Tl>(new Tl[] 
+                    {Tl.empty, Tl.grass_0, Tl.dirt_0, Tl.stone_0, Tl.gravel_0}));
+            }
+        }
+    }
+
+    void swap(int ind1, int ind2){
+        (int x, int y, HashSet<Tl> tiles) temp = queue[ind1];
+        queue[ind1] = queue[ind2];
+        queue[ind2] = temp;
+        indices[queue[ind1].x, queue[ind1].y] = ind1;
+        indices[queue[ind2].x, queue[ind2].y] = ind2;
+    }
+
+    public (int x, int y, HashSet<Tl> tiles) pop(){
+        (int x, int y, HashSet<Tl> tiles) output = queue[0];
+        queue[0] = queue[size];
+
+        int i = 0;
+        while(true){
+            int lCh = (2*i) + 1;
+            int rCh = (2*i) + 2;
+            if((lCh >= size || queue[i].tiles.Count <= queue[lCh].tiles.Count) &&
+                (rCh >= size || queue[i].tiles.Count <= queue[rCh].tiles.Count)){
+                break;
+            } else if(queue[i].tiles.Count > queue[lCh].tiles.Count){
+                swap(i, lCh);
+                i = lCh;
+            } else if(queue[i].tiles.Count > queue[rCh].tiles.Count){
+                swap(i, rCh);
+                i = rCh;
+            } else Debug.Log("WFCQueue.pop: Should not reach this control path");
+        }
+        size--;
+        return output;
+    }
+
+    public void update(int x, int y, HashSet<Tl> intersect){
+        int i = indices[x,y];
+        queue[i].tiles.IntersectWith(intersect);
+
+        while(true){
+            int par = (i-1)/2;
+            if(par < 0 || queue[i].tiles.Count >= queue[par].tiles.Count) break;
+            else{
+                swap(i, par);
+                i = par;
+            }
+        }
+    }
+
+    public void print(){
+        for(int i = 0; i < size; i++){
+            Debug.Log("x: " + queue[i].x + " y: " + queue[i].y + " Tiles: " + queue[i].tiles.Count);
+        }
+    }
 }
 
 
 class WaveFuncColl{
-    byte[,] map;
-    WFCRule[,] rules;
+    Tl[,] map;
+    WFCRule[][] rules;
+
+    public WaveFuncColl(int x, int y){ 
+        map = new Tl[x,y];
+        //Manually specifying rules for now
+        rules = new WFCRule[Enum.GetNames(typeof(Tl)).Length][];
+        rules[(int)Tl.grass_0] = new WFCRule[] {
+            new WFCRule(0, 1, new HashSet<Tl>(new Tl[] {Tl.empty})),
+            new WFCRule(0,-1, new HashSet<Tl>(new Tl[] {Tl.dirt_0, Tl.gravel_0})),
+            new WFCRule(1, 0, new HashSet<Tl>(new Tl[] {Tl.empty, Tl.grass_0, Tl.dirt_0, Tl.gravel_0})),
+            new WFCRule(-1,0, new HashSet<Tl>(new Tl[] {Tl.empty, Tl.grass_0, Tl.dirt_0, Tl.gravel_0}))
+        };
+        rules[(int)Tl.dirt_0] = new WFCRule[] {
+            new WFCRule(0, 1, new HashSet<Tl>(new Tl[] {Tl.grass_0, Tl.dirt_0, Tl.gravel_0})),
+            new WFCRule(0,-1, new HashSet<Tl>(new Tl[] {Tl.dirt_0, Tl.gravel_0})),
+            new WFCRule(1, 0, new HashSet<Tl>(new Tl[] {Tl.empty, Tl.grass_0, Tl.dirt_0, Tl.gravel_0})),
+            new WFCRule(-1,0, new HashSet<Tl>(new Tl[] {Tl.empty, Tl.grass_0, Tl.dirt_0, Tl.gravel_0}))
+        };
+        rules[(int)Tl.stone_0] = new WFCRule[] {
+            new WFCRule(0, 1, new HashSet<Tl>(new Tl[] {Tl.stone_0, Tl.gravel_0})),
+            new WFCRule(0,-1, new HashSet<Tl>(new Tl[] {Tl.stone_0})),
+            new WFCRule(1, 0, new HashSet<Tl>(new Tl[] {Tl.stone_0, Tl.gravel_0})),
+            new WFCRule(-1,0, new HashSet<Tl>(new Tl[] {Tl.stone_0, Tl.gravel_0}))
+        };
+        rules[(int)Tl.gravel_0] = new WFCRule[] {
+            new WFCRule(0, 1, new HashSet<Tl>(new Tl[] {Tl.grass_0, Tl.dirt_0})),
+            new WFCRule(0,-1, new HashSet<Tl>(new Tl[] {Tl.dirt_0, Tl.stone_0})),
+            new WFCRule(1, 0, new HashSet<Tl>(new Tl[] {Tl.grass_0, Tl.dirt_0, Tl.stone_0, Tl.gravel_0})),
+            new WFCRule(-1,0, new HashSet<Tl>(new Tl[] {Tl.grass_0, Tl.dirt_0, Tl.stone_0, Tl.gravel_0}))
+        };
+    }
+
+    public void run(){
+        return;
+    }
+
+
 }
