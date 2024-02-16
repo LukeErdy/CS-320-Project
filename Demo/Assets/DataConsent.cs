17e02abfba2d532444da04ab213c10ffbc3f0e69
Gabrielle-Barling <gabrielle.barling@wsu.edu>
2024-02-16 14:16:13 -0800

    Quit game
    I added a button inside the game scene to be able to quit the game. This will be need for collecting data on session start and end times. However, I cannot seem to figure out why the button doesn't actually show once game is started...?

--- Demo/Assets/DataConsent.cs
+++ Demo/Assets/DataConsent.cs
@@ -7,14 +7,17 @@ using Unity.Services.Analytics;
 
 public class DataConsent : MonoBehaviour
 { 
+    public bool consent;
    public void Consent()
     {
+        consent = true;
         UnityServices.InitializeAsync();
         AnalyticsService.Instance.StartDataCollection();
         SceneManager.LoadSceneAsync("Main Menu");
     }
    public void MainMenu()
     {
+        consent = false;
         SceneManager.LoadSceneAsync("Main Menu");
     }
     
