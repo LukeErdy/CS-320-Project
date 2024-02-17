7e03a9634c40cb9237db3cea6effa64fbfb80c5e
LukeErdy <112845457+LukeErdy@users.noreply.github.com>
2024-02-16 16:22:37 -0800

    Add an XP and health bar
    Player health and experience values represented as UI elements. Health is decremented when the player begins falling past a certain point. XP is awarded for pressing space. After the XP requirement is met, XP is reset to 0 and the requirement increases. These values can be manipulated by different game events with ease.

--- Demo/Assets/DeathCondition.cs
+++ Demo/Assets/DeathCondition.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Don't use this script. Handle adjustments to player health in PlayerMetrics.cs.
+
 public class DeathCondition : MonoBehaviour
 {
     float lowThreshold = -10f;
