e10f0652e66f712e62ca605a94fd017a3dcfbd41
amychisholm03 <amychisholm2003@gmail.com>
2024-02-19 11:42:25 -0800

    Updated spawner - Only spawns enemies on grass tiles - Spawner only runs after Generate finishes

--- Demo/Assets/EnemySpawn.cs
+++ Demo/Assets/EnemySpawn.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections;
+using System.Collections.Generic;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public int minEnemies = 0;
-    public int maxEnemies = 1;
-    public Tilemap spawnableTerrain;
-    public Tilemap unspawnableTerrain;
+    public int minEnemies;
+    public int maxEnemies;
+    public Generate generatedTerrain;
     public GameObject[] enemyPrefabs;
 
-    private void Start()
+    IEnumerator Start()
     {
-        if(enemyPrefabs == null || enemyPrefabs.Length == 0)
+        yield return new WaitUntil(() => generatedTerrain.IsInitialized);
+        if (enemyPrefabs == null || enemyPrefabs.Length == 0)
         {
             throw new System.ArgumentException("You must specify the enemy prefabs.");
         }
@@ -19,34 +21,27 @@ public class EnemySpawn : MonoBehaviour
         for (int i = 0; i < spawnCount; i++)
         {
             var randomPos = GetSpawnLocation();
+            //Debug.Log(randomPos);
             GameObject enemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
             var obj = Instantiate(enemyPrefab, randomPos, Quaternion.identity);
             obj.name = $"{enemyPrefab.name}{i+1}";
         }
+        //Debug.Log($"Spawned {spawnCount} enemies");
     }
 
     private Vector3 GetSpawnLocation()
     {
-        BoundsInt bounds = spawnableTerrain.cellBounds;
-        //Debug.Log($"SPAWNABLE xMin: {bounds.xMin}, xMax: {bounds.xMax} yMin: {bounds.yMin} yMax: {bounds.yMax}");
+        var grassTiles = generatedTerrain.grassTiles;
+        var tilemap = generatedTerrain.tilemap;
 
-        //BoundsInt dontSpawn = unspawnableTerrain.cellBounds;
-        //Debug.Log($"UNSPAWNABLE xMin: {bounds.xMin}, xMax: {bounds.xMax} yMin: {bounds.yMin} yMax: {bounds.yMax}");
+        //Select a random grass tile
+        var x = Random.Range(0, grassTiles.Length);
+        var y = grassTiles[x];
+        BoundsInt bounds = tilemap.cellBounds;
+        Vector3Int cell = new Vector3Int(x, y, 0);
 
-        // Generate a random position within the bounds
-        Vector3Int randomCell = new Vector3Int(
-            Random.Range(bounds.xMin, bounds.xMax),
-            Random.Range(bounds.yMin, bounds.yMax),
-            0
-        );
-
-        //https://docs.unity3d.com/ScriptReference/Tilemaps.Tilemap.html
-
-        // Get the center of the random cell
-        Vector3 pos = spawnableTerrain.GetCellCenterWorld(randomCell);
-
-        //Don't choose a position in the unspawnable terrain
-        if (unspawnableTerrain.GetTile(unspawnableTerrain.WorldToCell(pos))) return GetSpawnLocation();
+        //Get the center of the random cell
+        Vector3 pos = tilemap.GetCellCenterWorld(cell);
         return pos;
     }
 }
